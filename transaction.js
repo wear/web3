c890060751429bb15cf3a55456c5eec7e1988944
Stephen Kong <stephen.kong@fariaedu.com>
2021-06-04 21:08:09 +0800

    update trans js

--- transaction.js
+++ transaction.js
@@ -1,6 +1,6 @@
 const web3 = require('./base').ropsten()
 
-var Tx = require('ethereumjs-tx').Transaction
+
 const account1 = "0x4Ff0D037630e8794B7a054fDb38e9B7319f85901"
 const account2 = "0x160b74B15C3483648efcA3707D74593De86F8411"
 
@@ -8,6 +8,7 @@ const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1, 'hex')
 
 async function main() {
   const txCount = await web3.eth.getTransactionCount(account1)
+  
   const txObject = {
     nonce:    web3.utils.toHex(txCount),
     to:       account2,
@@ -16,12 +17,7 @@ async function main() {
     gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
   }
 
-  const tx = new Tx(txObject, { chain: 'ropsten', hardfork: 'petersburg' })
-  tx.sign(privateKey1)
-  
-  const serializedTx = tx.serialize()
-  const raw = '0x' + serializedTx.toString('hex')  
-
+  const raw = localSignTransaction(txObject, privateKey1)
   const txHash = await web3.eth.sendSignedTransaction(raw)
 
   console.log(`You can visit https://ropsten.etherscan.io/tx/${txHash['transactionHash']} for detail`)
@@ -32,4 +28,12 @@ main().catch((err) => {
   console.error(err)
 })
 
-
+function localSignTransaction(txObject, privateKey) {
+  let Tx = require('ethereumjs-tx').Transaction
+  const tx = new Tx(txObject, { chain: 'ropsten', hardfork: 'petersburg' })
+  tx.sign(privateKey)
+  
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')  
+  return raw
+}
